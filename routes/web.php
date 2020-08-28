0249439ca9f04b81457066464e110266596cd184
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-08-28 08:15:47 +0430

    add route category

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::namespace('Api\V1')->group( function() {
+    Route::resource('category', 'CategoryController');
 });
