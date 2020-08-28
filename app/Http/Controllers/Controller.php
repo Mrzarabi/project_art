a75a34e4b6a63d954e75b15d3120cddd8de7f670
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-08-28 08:04:22 +0430

    add method upload image

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public static function upload_image($image)
+    {
+        // Create file name & file path with /year/month/day/filename formats
+        $time = Carbon::now();   
+        $file_path = "uploads/{$time->year}/{$time->month}/{$time->day}";
+        $file_ext = $image->getClientOriginalExtension();
+        $file_name = rtrim($image->getClientOriginalName(), ".$file_ext");
+        $file_name = time() . '_' . substr($file_name, 0, 30);
+        
+        // Create directories if doesn't exists
+        if (!file_exists( public_path($file_path) )) {
+            mkdir(public_path($file_path), 0777, true);
+        }
+
+        $image->move( public_path("$file_path/$file_name.$file_ext") );
+        return "/$file_path/$file_name.$file_ext";
+    }
 }
