b9561726702fb6b611de7bf442f1f35063d536fd
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:32:22 +0330

    add some new routes for api

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->namespace('Api\V1')->group( function () {
-    
+Route::prefix('v1')
+    ->namespace('Api\V1')
+    ->group( function() {
+
+        Route::post('login', 'UserController@login');
+    });
+
+/**
+ * This route group for user who has role 100... and he/she must logined at the first
+ */
+Route::middleware(['auth:api', 'role:100e82ba-e1c0-4153-8633-e1bd228f7399'])
+    ->prefix('v1')
+    ->namespace('Api\V1')
+    ->group( function () {
+        
+    Route::get('user/{user}', 'UserController@show');
+    Route::put('user/{user}', 'UserController@update');
+
     Route::resource('category', 'CategoryController');
+
+    Route::resource('post', 'PostController');
+    Route::post('post/upload/{post}', 'PostController@upload');
+    Route::put('post/update/upload/{post}', 'PostController@updateUpload');
+    Route::post('post/multi/delete', 'PostController@multiDelete');
+
+    Route::resource('exhibition', 'ExhibitionController');
+    Route::post('exhibition/upload/{exhibition}', 'ExhibitionController@upload');
+    Route::put('exhibition/update/upload/{exhibition}', 'ExhibitionController@updateUpload');
+    Route::post('exhibition/multi/delete', 'ExhibitionController@multiDelete');
+    
+    Route::resource('event', 'EventController');
+    Route::post('event/multi/delete', 'EventController@multiDelete');
+    
+    Route::resource('praised', 'PraisedController');
+    Route::post('praised/upload/{praised}', 'PraisedController@upload');
+    Route::put('praised/update/upload/{praised}', 'PraisedController@updateUpload');
+    Route::post('praised/multi/delete', 'PraisedController@multiDelete');
 });
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::prefix('free/v1')
+    ->namespace('Api\V1')
+    ->group( function() {
+        
+});
