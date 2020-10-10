b3f7950f5db5ece2a1d6810bf3b9a39baf32d81e
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:29:06 +0330

    create model user with relations

--- app/User.php
+++ app/User.php
@@ -2,13 +2,19 @@
 
 namespace App;
 
+use App\models\Event;
+use App\Models\Exhibition;
+use App\Models\Post;
+use App\Models\Praised;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, LaratrustUserTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +22,17 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'avatar',
+        'name',
+        'family',
+        'phone_number',
+        'i_acc',
+        'f_acc',
+        'address',
+        'desc',
+        'email',
+        'password',
+        'api_token',
     ];
 
     /**
@@ -25,7 +41,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'api_token',
     ];
 
     /**
@@ -36,4 +52,71 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if ( ! $model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    /**
+     * Relations
+     */
+    
+    /**
+     * Each User can has many posts
+     */
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
+
+    /**
+     * Each User can has many events
+     */
+    public function events() {
+        return $this->hasMany(Event::class);
+    }
+
+    /**
+     * Each User can has many exhibitions
+     */
+    public function exhibitions() {
+        return $this->hasMany(Exhibition::class);
+    }
+
+    /**
+     * Each User can has many praised
+     */
+    public function praiseds() {
+        return $this->hasMany(Praised::class);
+    }
 }
