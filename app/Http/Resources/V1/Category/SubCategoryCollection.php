16380e03b415587c21f4001e215897e0a087d348
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:34:27 +0330

    add new codes

---
+++ app/Http/Resources/V1/Category/SubCategoryCollection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\V1\Category;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class SubCategoryCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection->map( function($item) {
+                return [
+                    'id'    => $item->id,
+                    'title' => $item->title,
+                    'desc' => $item->desc,
+                    'image' => $item->image,
+                    'time' => $item->created_at->format('d M Y'),
+                    'children' => new SubCategoryCollection($item->categories),
+                ];
+            })
+        ];
+    }
+}
