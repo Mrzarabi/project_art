8a7097d67f080496c08a265c1bfd35bd6a5e34a2
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:33:20 +0330

    add new request

---
+++ app/Http/Requests/V1/Praised/PraisedRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\V1\Praised;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PraisedRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'nullable|string',
+            'desc' => 'nullable|string',
+            'body' => 'nullable|string',
+            's_link' => 'nullable|string',
+            'date' => 'nullable|string',
+        ];
+    }
+}
