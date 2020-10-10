0bd15e5260a5cd790ed80deb963fc52e36502c17
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:32:01 +0330

    add new request

---
+++ app/Http/Requests/V1/Exhibition/ExhibitionRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\V1\Exhibition;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ExhibitionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'nullable|string',
+            'desc' => 'nullable|string',
+            'body' => 'nullable|string',
+            'i_link' => 'nullable|string',
+            'f_link' => 'nullable|string',
+            's_link' => 'nullable|string',
+            'date' => 'nullable|string',
+            'address' => 'nullable|string',
+        ];
+    }
+}
