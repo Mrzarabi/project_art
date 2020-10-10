2f55d4ecb27598fb96142eef65e4c36cafd61715
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:44:33 +0330

    add new table

---
+++ database/migrations/2020_10_02_082353_create_events_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->string('image')->nullable();
+            $table->string('title')->nullable();
+            $table->string('desc')->nullable();
+            $table->text('body')->nullable();
+            $table->string('i_link')->nullable();
+            $table->string('f_link')->nullable();
+            $table->string('date')->nullable();
+            $table->string('address')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
