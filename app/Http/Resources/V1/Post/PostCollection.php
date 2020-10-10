832272d38368b61edb1cac9680627b786f88e8e7
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:37:38 +0330

    add new collection

---
+++ app/Http/Resources/V1/Post/PostCollection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources\V1\Post;
+
+use App\Http\Resources\V1\Image\ImageCollection;
+use App\Http\Resources\V1\Video\VideoCollection;
+use App\Models\Image;
+use App\Models\Video;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection->map( function($item) {
+                return [
+                    'id' => $item->id,
+                    'category' => $item->category_id ? $item->category->title : '',
+                    'title' => $item->title,
+                    'desc' => $item->desc,
+                    'body' => $item->body,
+                    'time' => $item->created_at->format('d M Y'),
+                    'images'   => new ImageCollection(Image::where('post_id', $item->id)->get()),
+                    'videos' => new VideoCollection(Video::where('post_id', $item->id)->get()),
+                ];
+            })
+        ];
+    }
+}
