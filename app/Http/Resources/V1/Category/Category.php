de257bae6ef8a1e30596a6da6d8e97bcd756a466
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:34:07 +0330

    add new codes for response

--- app/Http/Resources/V1/Category/Category.php
+++ app/Http/Resources/V1/Category/Category.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources\V1\Category;
 
+use App\Http\Resources\V1\Post\PostCollection;
+use App\Models\Post;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Category extends JsonResource
@@ -15,10 +17,13 @@ class Category extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'desc' => $this->desc,
             'image' => $this->image,
-            // 'children' => new SubCategoryCollection($this->categories)
+            'children' => new SubCategoryCollection($this->categories),
+            'time' => $this->created_at->format('d M Y'),
+            'posts' => new PostCollection( Post::where('category_id', $this->id)->get() ),
         ];
     }
 }
