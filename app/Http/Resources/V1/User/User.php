a292324cb99b885e2b444eaa22b4dd84e50af511
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:36:51 +0330

    add new resource

---
+++ app/Http/Resources/V1/User/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\V1\User;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class User extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'   => $this->id,
+            'avatar'   => $this->avatar,
+            'name'   => $this->name,
+            'family'   => $this->family,
+            'phone_number'   => $this->phone_number,
+            'i_acc'   => $this->i_acc,
+            'f_acc'   => $this->f_acc,
+            'address'   => $this->address,
+            'desc'   => $this->desc,
+            'email' => $this->email,
+            'api_token' => $this->api_token,
+        ];
+    }
+}
