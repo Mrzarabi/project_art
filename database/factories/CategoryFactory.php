4c63c9c606f8b1528e4ce3722487fce296cd32fb
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:43:24 +0330

    test

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,12 +3,21 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Category;
+use App\Models\Post;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
     return [
         'title' => $faker->firstName(),
-        'desc' => $faker->paragraph(150),
+        'desc' => $faker->sentence(10),
         'image' => $faker->imageUrl(300, 300)
+    ];
+});
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->firstName(),
+        'desc' => $faker->sentence(10),
+        'body' => $faker->sentence(10)
     ];
 });
