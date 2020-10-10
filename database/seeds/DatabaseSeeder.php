ed2c6ec3dadc6dd79a90afe5975ddd83f49ede3d
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:46:38 +0330

    comment extra codes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $categories = factory(Category::class, 10)->create();
-        $categories->each( function($category) {
-            $category->categories()->saveMany( 
-                factory(Category::class, rand(0,5))->make()
-             );
-        });
+        
+        // $categories = factory(Category::class, 10)->create();
+        // $categories->each( function($category) {
+        //     $category->categories()->saveMany( 
+        //         factory(Category::class, rand(0,5))->make()
+        //     );
+        // });
+
+        // $categories->each( function($category) {
+        //     $category->posts()->saveMany(
+        //         factory(Post::class, rand(0, 4) )->make()
+        //     );
+        // });
+        $this->call(LaratrustSeeder::class);
     }
 }
