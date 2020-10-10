d240ff4cb8a45bd535dc99bcc1e00f8bbd235019
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-10 23:37:49 +0330

    add new collection

---
+++ app/Http/Resources/V1/Video/VideoCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\V1\Video;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class VideoCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection->map( function($item) {
+                return [
+                    'id' => $item->id,
+                    'videoUrl' => $item->videoUrl
+                ];
+            })
+        ];
+    }
+}
