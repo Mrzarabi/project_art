71400c8c3df6d7cb545a4be0f08337c4f745d10d
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-12-03 10:48:13 +0330

    delete file image from folder public

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Intervention\Image\ImageManagerStatic as Image;
 
 class Controller extends BaseController
 {
@@ -22,5 +22,18 @@ class Controller extends BaseController
         $image->move(public_path($imagePath) , $filename);
         $file = $imagePath . $filename;
         return $file;
+    }
+
+    public static function unlink_image($id) 
+    {
+        $filename = Image::find( $id );
+        $path = public_path($filename->image);
+        return $path;
+    }
+
+    public static function unlink_image_from_path($path)
+    {
+        $path = public_path($path);
+        return $path;
     }
 }
